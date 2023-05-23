0fd6f97ecda3e07440fc53736b94b9bd27f9c573
KshitijaDighe <97294073+KshitijaDighe@users.noreply.github.com>
2023-05-23 16:15:34 +0530

    commited

---
+++ hello.c
@@ -0,0 +1,9 @@
+##include <stdio.h>    
+int main(){    
+printf("Hello C Language");    
+return 0;   
+}  include <stdio.h>    
+int main(){    
+printf("Hello C Language");    
+return 0;   
+}  
